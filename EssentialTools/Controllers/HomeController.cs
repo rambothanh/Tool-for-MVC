51e4c5445ea0553543ca6642e896041cbae374ca
Nguyen Trong Thanh <rambothanh1985@gmail.com>
2018-12-01 18:21:53 +0700

    Setting up MVC Dependency Injection

--- EssentialTools/Controllers/HomeController.cs
+++ EssentialTools/Controllers/HomeController.cs
@@ -4,12 +4,23 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using EssentialTools.Models;
+using Ninject;
 
 namespace EssentialTools.Controllers
 {
     public class HomeController : Controller
     {
+        //add a class constructor that accepts
+        //an implementation of the IValueCalculator interface,
+        private IValueCalculator calc;
+        public HomeController(IValueCalculator calcParam)
+        {
+            calc = calcParam;
+        }
+        
         private Product[] products = {
+            
+
             new Product {Name = "Kayak", Category = "Watersports", Price = 275M},
             new Product {Name = "Lifejacket", Category = "Watersports", Price = 48.95M},
             new Product {Name = "Soccer ball", Category = "Soccer", Price = 19.50M},
@@ -19,9 +30,36 @@ namespace EssentialTools.Controllers
         // GET: Home
         public ActionResult Index()
         {
+            ////đầu tiên là chuẩn bị Ninject để sử dụng.
+            ////Để làm điều này, tôi tạo một thể hiện của Ninject Kernel,
+            ////là đối tượng chịu trách nhiệm giải quyết các phụ thuộc
+            ////và tạo các đối tượng mới. Khi tôi cần một đối tượng,
+            ////tôi sẽ sử dụng Kernel thay vì từ khóa new.
 
-            IValueCalculator calc = new LinqValueCalculator();
-            ShoppingCart cart = new ShoppingCart(calc) {Products = products};
+            ////creating a new instance of the StandardKernel class
+            //IKernel ninjectKernel = new StandardKernel();
+
+            ////Thiết lập interface muốn làm việc với tham số kiểu cho phương thức Bind
+            ////Và gọi phương thức To trên kết quả return
+            ////Thiết lập Inlementation class muốn khởi tạo bằng tham số kiểu cho ư
+            ////phương thức To
+            ////Câu lệnh này nói với Ninject rằng các phụ thuộc trên
+            ////IValueCalculator interface nên được giải quyết bằng cách
+            ////tạo một cá thể của lớp LinqValueCalculator
+            //ninjectKernel.Bind<IValueCalculator>().To<LinqValueCalculator>();
+
+            ////Sử dụng Ninject để tạo một đối tượng thông qua phương thức Get
+            ////Tham số kiểu của phương thức Get nói với Ninject biết rằng 
+            ////interface nào mà tôi quan tâm. 
+            ////Phương thức Get này trả về kiểu triển khai (implementation type)
+            //// mà tôi đã chỉ định với phương thức To ở bên trên.
+            //IValueCalculator calc = ninjectKernel.Get<IValueCalculator>();
+
+            //Những dòng comment bên trên đây đã được xử lý bằng:
+            //Setting up MVC Dependency Injection
+            //Đã phá vỡ được sự phụ thuộc của lớp LinqValueCalculator và lớp
+            //HomeController 
+            ShoppingCart cart = new ShoppingCart(calc) { Products = products };
             decimal totalValue = cart.CalculateProductTotal();
 
             return View(totalValue);
