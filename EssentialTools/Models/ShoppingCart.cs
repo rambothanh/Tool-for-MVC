fac5c1484c1abb76fbfcaf475932867acf40535a
Nguyen Trong Thanh <rambothanh1985@gmail.com>
2018-12-01 16:31:39 +0700

    init commit

---
+++ EssentialTools/Models/ShoppingCart.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EssentialTools.Models
+{
+    public class ShoppingCart
+    {
+        //Tạo trường kiểu Linq...
+        private LinqValueCalculator cals;
+        
+        //Hàm khởi tạo
+        public ShoppingCart(LinqValueCalculator calcParam)
+        {
+            cals = calcParam;
+        }
+
+        //Thuộc tính kiểu IEnumerable<Product>
+        public IEnumerable<Product> Products { get; set; }
+
+        //Method tính tổng
+        public decimal CalculateProductTotal()
+        {
+            return cals.ValueProducts(Products);
+        }
+
+    }
+}
